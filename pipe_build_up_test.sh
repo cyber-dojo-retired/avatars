7e2b1fe7c56ce8be971874b163d5d4341145acdf
JonJagger <jon@jaggersoft.com>
2019-08-07 14:01:01 +0100

    [ci skip] update main script

--- pipe_build_up_test.sh
+++ pipe_build_up_test.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 if [ "${1}" = '-h' ] || [ "${1}" = '--help' ]; then
   echo
@@ -11,13 +10,22 @@ fi
 
 readonly SH_DIR="$( cd "$( dirname "${0}" )" && pwd )/sh"
 
-${SH_DIR}/build_docker_images.sh "$@"
-${SH_DIR}/docker_containers_up.sh "$@"
+if ! ${SH_DIR}/build_docker_images.sh "$@" ; then
+  exit 3
+fi
+
+if ! ${SH_DIR}/docker_containers_up.sh "$@" ; then
+  exit 3
+fi
+
 ${SH_DIR}/run_tests_in_containers.sh "$@"
+
 if [ "${AVATARS_COVERAGE}" != 'off' ]; then
   open file://${SH_DIR}/../test_server/coverage/index.html
 fi
+
 if [ "${AVATARS_DEMO}" != 'off' ]; then
   ${SH_DIR}/demo_html.sh
 fi
+
 ${SH_DIR}/docker_containers_down.sh
