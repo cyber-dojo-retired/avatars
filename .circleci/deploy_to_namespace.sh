b91615eae1b873b9a1acfb57de7bfeefd78d69e7
Jon Jagger <jon@jaggersoft.com>
2020-05-24 13:11:13 +0100

    Try to fix avatars deployment - currently only on one node

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -Eeu
 
+# Normally I export the cyberdojo env-vars using the command
+# $ docker run --rm cyberdojo/versioner:latest
+# This won't work on the.circleci deployment step since it is
+# run inside the cyberdojo/gcloud-kubectl-helm image which does
+# not have docker. So doing it directly from versioner's git repo
+export $(curl https://raw.githubusercontent.com/cyber-dojo/versioner/master/app/.env)
+
 readonly NAMESPACE="${1}" # beta | prod
+readonly IMAGE="${CYBER_DOJO_AVATARS_IMAGE}"
+readonly PORT="${CYBER_DOJO_AVATARS_PORT}"
+readonly TAG="${CIRCLE_SHA1:0:7}"
 
 # misc env-vars are in ci context
 
@@ -16,12 +26,19 @@ gcloud container clusters get-credentials \
   --project "${PROJECT}"
 
 helm init --client-only
+
 helm repo add praqma https://praqma-helm-repo.s3.amazonaws.com/
+
 helm upgrade \
   --install \
   --namespace=${NAMESPACE} \
-  --set-string containers[0].tag=${CIRCLE_SHA1:0:7} \
-  -f .circleci/avatars-values.yaml \
+  --set-string containers[0].image=${IMAGE} \
+  --set-string containers[0].tag=${TAG} \
+  --set service.port=${PORT} \
+  --set containers[0].livenessProbe.port=${PORT} \
+  --set containers[0].readinessProbe.port=${PORT} \
+  --set-string service.annotations."prometheus\.io/port"=${PORT} \
+  --values .circleci/avatars-values.yaml \
   ${NAMESPACE}-avatars \
   praqma/cyber-dojo-service \
   --version 0.2.5
