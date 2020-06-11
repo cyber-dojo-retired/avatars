66d8e96065808383bb768247c030f48edcada304
Jon Jagger <jon@jaggersoft.com>
2020-06-11 18:14:12 +0100

    Make specific k8s yml values last argument and avoid need for empty file

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -31,11 +31,8 @@ readonly NAMESPACE="${1}" # beta | prod
 readonly CYBER_DOJO_AVATARS_TAG="${CIRCLE_SHA1:0:7}"
 
 helm_upgrade \
-   "${NAMESPACE}" \
+   "${NAMESPACE}" "avatars" "praqma/cyber-dojo-service --version 0.2.5" \
    "${CYBER_DOJO_AVATARS_IMAGE}" \
    "${CYBER_DOJO_AVATARS_TAG}" \
    "${CYBER_DOJO_AVATARS_PORT}" \
-   ".circleci/k8s-general-values.yml" \
-   ".circleci/k8s-specific-values.yml" \
-   "avatars" \
-   "praqma/cyber-dojo-service --version 0.2.5"
+   ".circleci/k8s-general-values.yml"
