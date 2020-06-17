25bd8a9f20de740947a2ffc30912f170fd690e00
Jon Jagger <jon@jaggersoft.com>
2020-06-17 17:53:55 +0100

    Use refactored k8s deployment function

--- .circleci/deploy_to_namespace.sh
+++ .circleci/deploy_to_namespace.sh
@@ -9,7 +9,7 @@ readonly CYBER_DOJO_AVATARS_TAG="${CIRCLE_SHA1:0:7}"
 
 gcloud_init
 helm_init
-helm_upgrade_probe_yes_prometheus_yes \
+helm_upgrade \
    "${NAMESPACE}" \
    "avatars" \
    "${CYBER_DOJO_AVATARS_IMAGE}" \
